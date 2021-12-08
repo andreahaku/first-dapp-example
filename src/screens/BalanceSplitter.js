19db5e3bee663998ef0b5cd514d2c4cf727c2fad
Andrea Salvatore <andrea.salvatore@consensys.net>
2021-12-08 12:22:12 +0100

    adds account balancing with ETH transaction

--- src/screens/BalanceSplitter.js
+++ src/screens/BalanceSplitter.js
@@ -23,11 +23,38 @@ const BalanceSplitter = () => {
   const [accounts, setAccounts] = useState([initialAccount, initialAccount]);
 
   const [errorMessage, setErrorMessage] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
 
   // automatically hides any error message after 2 seconds
   useEffect(() => {
     setTimeout(() => setErrorMessage(false), 2000);
   }, [errorMessage]);
+
+  // requests permission to wallet to select a new account
+  const requestNewAccount = async () => {
+    const { ethereum } = window;
+
+    await ethereum.request({
+      method: "wallet_requestPermissions",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+
+    // asks to select a new account
+    const account = await ethereum.request({
+      method: "eth_requestAccounts",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+
+    return account;
+  };
 
   // selects the account from wallet
   const selectAccount = async (isFirst = true, isReset = false) => {
@@ -41,26 +68,9 @@ const BalanceSplitter = () => {
         method: "eth_requestAccounts",
       });
 
-      // requests permission to wallet to select a new account
       if (!isFirst || isReset) {
-        await ethereum.request({
-          method: "wallet_requestPermissions",
-          params: [
-            {
-              eth_accounts: {},
-            },
-          ],
-        });
-
         // asks to select a new account
-        account = await ethereum.request({
-          method: "eth_requestAccounts",
-          params: [
-            {
-              eth_accounts: {},
-            },
-          ],
-        });
+        account = await requestNewAccount();
       }
 
       // gets the balance of the selected account
@@ -83,9 +93,56 @@ const BalanceSplitter = () => {
   };
 
   // splits the balance between the two accounts
-  const splitBalance = () => {
-    console.log("SPLIT");
+  const splitBalance = async () => {
+    // average balance between the two accounts
+    const average = (accounts[FIRST].balance + accounts[SECOND].balance) / 2;
+
+    // which is the larger one?
+    const isFirstBalanceGreater =
+      accounts[FIRST].balance > accounts[SECOND].balance;
+
+    const fromAccount = accounts[isFirstBalanceGreater ? FIRST : SECOND].id;
+    const toAccount = accounts[isFirstBalanceGreater ? SECOND : FIRST].id;
+
+    // calculates transaction value to balance accounts
+    const transactionValue =
+      average - accounts[isFirstBalanceGreater ? SECOND : FIRST].balance;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    if (isFirstBalanceGreater) {
+      // asks to select the first account to sign the transaction
+      // this is needed when the first account balance is larger that
+      // the first one, and the transaction is from first > second
+      alert(
+        `Please select the first account:\n\nAccount: ${accounts[FIRST].id}\nBalance: ${accounts[FIRST].balance} ETH`
+      );
+
+      requestNewAccount();
+    }
+
+    try {
+      const tx = await signer.sendTransaction({
+        from: fromAccount,
+        to: toAccount,
+        value: ethers.utils.parseEther(transactionValue.toString()),
+      });
+
+      setSuccessMessage(`TRANSACTION SUCCESSFULL:\n${tx.hash}`);
+
+      // restarts the dapp after 5 seconds
+      setTimeout(() => {
+        setSuccessMessage(false);
+        window.location.reload();
+      }, 5000);
+    } catch (error) {
+      console.warn("ERROR:", error);
+    }
   };
+
+  // reloads the dapp if user changes network
+  window.ethereum?.on("chainChanged", () => window.location.reload());
 
   return (
     <>
@@ -134,16 +191,15 @@ const BalanceSplitter = () => {
             <h3>{"Average Balance:"}</h3>
             <Spacer width={10} />
             <p>{`${
-              (parseFloat(accounts[FIRST].balance) +
-                parseFloat(accounts[SECOND].balance)) /
-              2
+              (accounts[FIRST].balance + accounts[SECOND].balance) / 2
             } ETH`}</p>
           </Row>
           <ConnectButton label={"Split Balance"} onClick={splitBalance} />
         </>
       )}
 
-      {<ErrorMessage>{errorMessage}</ErrorMessage>}
+      {successMessage && <SuccessMessage>{successMessage}</SuccessMessage>}
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
     </>
   );
 };
@@ -152,6 +208,10 @@ export default BalanceSplitter;
 
 const ErrorMessage = styled.h4`
   color: red;
+`;
+
+const SuccessMessage = styled.h3`
+  color: green;
 `;
 
 const CardsContainer = styled.div`
